9673f4913881d1ea27a7444475abce12964595b8
yuk0624 <mapiharuioxmasayuka@gmail.com>
2022-04-15 16:40:12 +0900

    change https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        if(\App::enviroment(['production'])){
+            \URL::forceScheme('https');
+        }
     }
 }
