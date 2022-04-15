5a166bd2fb720e1ecf814a1095b16c334fd43f60
yuk0624 <mapiharuioxmasayuka@gmail.com>
2022-04-15 12:42:57 +0900

    firstcommit

---
+++ app/Policies/TaskPolicy.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Task;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class TaskPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    // 指定されたユーザーのタスクの時削除可能
+
+    // @param User $user
+    // @param Task $task
+    // @returm bool
+
+    public function destroy(User $user,Task $task)
+    {
+        return $user->id === $task->user_id;
+    }
+}
